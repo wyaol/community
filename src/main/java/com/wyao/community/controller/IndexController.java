33c98375e92e3035fbca80f41e14e4ca990d4c07
wyao <289672494@qq.com>
2019-10-15 19:55:12 +0800

    add bootstrap and index

---
+++ src/main/java/com/wyao/community/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.wyao.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/index")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+}
